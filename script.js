9caa0b4d7e5a7e45517fc2f8ac1ff0f336f8f050
eeshunarayan15 <106856120+eeshunarayan15@users.noreply.github.com>
2024-08-19 09:21:40 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,38 @@
+
+
+
+gsap.set(".a",{opacity:0,y:10})
+gsap.set("#right img",{opacity:0,y:10,scale:1.6})
+
+
+var tl=gsap.timeline();
+
+tl.from("#left",{
+width:0,
+ease :Expo.easeInOut,
+    duration: 1.5,
+})
+.from("#right",{
+    width:0,
+    ease :Expo.easeInOut,
+        duration: 1.5,
+    })
+    .to(".a",{
+        delay:-2,
+        stagger:.1,
+        opacity:1,
+        y:0,
+    
+        ease :Expo.easeInOut,
+            duration: 2,
+        })
+        .to("#right img",{
+            delay:-2,
+          
+            opacity:1,
+            y:0,
+            scale:1,
+        
+            ease :Expo.easeInOut,
+                duration: 2,
+            })
